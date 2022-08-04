59dbb7daaa22256131212ae012eba5e589abbdb3
killshotxd <hassanansari211@gmail.com>
2022-08-04 17:20:29 +0530

    Project structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+        <List />
+        <button onClick={() => console.log("You Click me")}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
